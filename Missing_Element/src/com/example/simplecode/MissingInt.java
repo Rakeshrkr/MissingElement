ce4c997a39257be9ef922fce5ec96f9a6cc020af
Rakeshrkr <jrakesh.iitb@gmail.com>
2016-08-10 09:11:51 +0530

    Add comment

--- Missing_Element/src/com/example/simplecode/MissingInt.java
+++ Missing_Element/src/com/example/simplecode/MissingInt.java
@@ -5,7 +5,7 @@ public class MissingInt {
 		int array_sum = 0 ; 
 		int array[] = {5 , 8 , 6 , 7 , 1 , 9 , 2 , 3 ,8} ; // array of length 9
 		for(int i=0 ; i< array.length ; i++){
-			array_sum +=  array[i] ;
+			array_sum +=  array[i] ; // summing the elements of the array
 		}
 			int real_sum = 10*11/2 ;
 			int missing_int = real_sum - array_sum ;
